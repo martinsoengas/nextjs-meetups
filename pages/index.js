9edbfee62e5f31377114eb224873aa53f2c97333
Martin Soengas <martinsoengas@Martins-MacBook.local>
2022-06-23 19:15:54 -0300

    added hardcoded dummy server-side rendering

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,25 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-const HomePage = () => {
+const HomePage = (props) => {
   return <MeetupList meetups={DUMMY_MEETUPS} />;
 };
+
+// export function getServerSideProps(context) {
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 10,
+  };
+}
 
 export default HomePage;
